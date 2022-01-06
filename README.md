93ca7889c885033c9fc204b87f22df831fbeff82
May0505 <mayy.luo@gmail.com>
2022-01-06 18:21:13 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,17 @@
+# 餐廳清單
+
+使用 Node.js + Express 製作的餐廳網站，提供瀏覽餐廳及搜尋餐廳的功能。
+
+## Features - 功能列表
+
+- 顯示餐廳列表
+- 從列表點擊後，顯示餐廳詳細資料（餐廳名稱、餐廳類別、評價、地址、圖片、電話）
+- 搜尋餐廳名稱及餐廳類別
+
+## 專案畫面
+
+![MyImage](/public/image/homePage.png)
+
+![MyImage](/public/image/detail.png)
+
+## Environment SetUp - 環境建置
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { lists: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurantList_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(restaurant => restaurant.id === Number(req.params.restaurantList_id))
+  res.render('show', { restaurant: restaurant[0]})
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+  const restaurants = restaurantList.results.filter((restaurant) => {
+  return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword) 
+})
+  res.render('index', { lists: restaurants, keyword:req.query.keyword})
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
